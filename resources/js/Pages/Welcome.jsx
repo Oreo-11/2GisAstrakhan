2e4434fa5277aa374c769e3d64598d7436acfb6c
Oreo-11 <80238741+Oreo-11@users.noreply.github.com>
2025-04-25 12:30:42 +0400

    Оп

--- resources/js/Pages/Welcome.jsx
+++
@@ -1,9 +0,0 @@
-import { Link, Head } from '@inertiajs/react';
-
-export default function Welcome({ auth, laravelVersion, phpVersion }) {
-    return (
-        <>
-        <h1>68767</h1>
-        </>
-    );
-}
