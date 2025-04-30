a06586af9015980da5a934a060a43d7e664c20ea
Mojusei <metra9991@mail.ru>
2025-04-30 22:04:34 +0400

    + к предыдущему коммиту

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
@@ -13,5 +14,10 @@ class User extends Model
     public function images() : HasMany
     {
         return $this->hasMany(UserImage::class);
+    }
+
+    public function favourites() : BelongsToMany
+    {
+        return $this->belongsToMany(Restaraunt::class, 'users_favourite_restaraunts', 'user_id', 'restaraunt_id');
     }
 }
